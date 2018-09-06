920690855ee5018029dcbecaf3a30e92ce08e92c
Austin DeLaRosa <austin.delarosa@gmail.com>
2018-09-05 20:07:37 -0400

    Works page updated new content
    New content on works page, mainly web app

--- austin-website/austin-website/obj/Release/Package/PackageTmp/Content/mailer/form_setting.php
+++ austin-website/austin-website/obj/Release/Package/PackageTmp/Content/mailer/form_setting.php
@@ -1,6 +1,6 @@
 <?php
 	/*Form settings*/
-	$subj = "New message from 'Austin DeLaRosa website'"; //letter subject
+	$subj = "New message from 'Austin DeLaRosa website"; //letter subject
 	$to = 'austin.delarosa@gmail.com'; // Enter Your E-mail
 	$from = 'austin.delarosa@gmail.com'; // Admin e-mail
 	$fromName = 'DeLaRosa Productions, Ltd.'; // Your company name
